2baf24162358ed9045eea2a2f3ef33e4f2f83227
Pulkit Kumar Agarwal <63954083+pka5667@users.noreply.github.com>
2023-10-24 16:21:26 +0530

    Replace commands with reply buttons in bot

--- python_modules/telegram_bot.py
+++ python_modules/telegram_bot.py
@@ -1,12 +1,12 @@
 # PIP INSTALL python-telegram-bot 
 
-from telegram import Update, InlineKeyboardMarkup, InlineKeyboardButton
+from telegram import Update, InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, CallbackQueryHandler
 from telegram.ext import filters , MessageHandler
 
 # from langchain.llms import OpenAI
 from .llm_prompts import LLM
-from .scrape_products import ChromeDriver
+from .chrome_driver import ChromeDriver
 
 
 
@@ -16,43 +16,53 @@ class TelegramBot:
     def __init__(self, token: str, llm: LLM, driver: ChromeDriver) -> None:
         self.app = ApplicationBuilder().token(token).build()
         self.app.add_handler(MessageHandler(filters.TEXT & (~filters.COMMAND), self.message_handler))
-        self.app.add_handler(CommandHandler("hello", self.start))
+        # self.app.add_handler(CommandHandler("hello", self.start))
         self.app.add_handler(CommandHandler("start", self.start))
-        self.app.add_handler(CommandHandler("get_products", self.get_products))
-        self.app.add_handler(CommandHandler("reset_search_query", self.reset_search_query))
+        # self.app.add_handler(CommandHandler("get_products", self.get_products))
+        # self.app.add_handler(CommandHandler("reset_search_query", self.reset_search_query))
 
         self.app.add_handler(CallbackQueryHandler(self.get_product_details))
-
+        
         self.llm = llm
         self.driver = driver
 
+        self.reply_markup_remove = ReplyKeyboardRemove()
+
+        ################################# MESSAGES #################################
+        self.hello_message = ('Hello {name}, \n'
+                                'I am here to help you with searching the products on amazon based on your requirements. '
+                                'Please tell me what you are looking for.')
+        
+        self.get_product_details_message = ('Getting details for product with ASIN: {product_asin}. \n\n'
+                                            "This can be implemented better using API, but for now you can open this link to see the product details:\n" + \
+                                            "https://www.amazon.in/dp/{product_asin}")
+        self.get_products_empty_query_message = ('Your search query is empty, ' 
+                                                'before getting products from amazon please tell me what you are looking for.\n\n'
+                                                'For example: Show me some laptops in range Rs. 30000 to 40000')
+        self.reset_search_query_message = ('Hello {first_name}\n'
+                                            'Your query has been reset, please tell me what you want to search on Amazon')
+        self.message_handler_message = ('{query}\n\n'
+                                            'Is this what you are looking for {name}?\n')
 
 
+
+    ################################# FUNCTIONS #################################
     async def start(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         if update.effective_user.id not in self.queries or self.queries[update.effective_user.id] != "":
             self.queries[update.effective_user.id] = ""
-        print("Bot started by: ", update.effective_user.first_name, update.effective_user.id)
-        await update.message.reply_text(f'Hello {update.effective_user.first_name}, \n'
-                                        'I am here to help you with searching the products on amazon based on your requirements. '
-                                        'Please tell me what you are looking for.')
+        print("Bot started by: ", update.effective_user.username, update.effective_user.id)
+        await update.message.reply_text(self.hello_message.format(name=update.effective_user.first_name),
+                                        reply_markup=self.reply_markup_remove)
     
-
-    async def get_product_details(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
-        product_asin = update.callback_query.data
-        # product_details = self.driver.get_product_details(product_asin)
-        await update.callback_query.message.reply_text(f'Getting details for product with ASIN: {product_asin}. \n\n'
-                                                       "This can be implemented better using API, but for now you can open this link to see the product details:\n" + \
-                                                       f"https://www.amazon.in/dp/{product_asin}")
-
 
 
     async def get_products(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         if update.effective_user.id not in self.queries or self.queries[update.effective_user.id] == "":    
-            await update.message.reply_text('Your search query is empty, ' 
-                                            'before getting products from amazon please tell me what you are looking for.\n\n'
-                                            'For example: Show me some laptops in range Rs. 30000 to 40000')
+            await update.message.reply_text(self.get_products_empty_query_message, 
+                                            reply_markup=self.reply_markup_remove)
         else:
-            await update.message.reply_text(f'Getting Products for query:\n{self.queries[update.effective_user.id]}')
+            await update.message.reply_text(f'Getting Products for query:\n{self.queries[update.effective_user.id]}', 
+                                            reply_markup=self.reply_markup_remove)
             data = self.driver.scrape_products(self.queries[update.effective_user.id])
 
             scraped_data_message = "\n\n".join([str((i+1)) + ". " + str(data['product_names'][i]) for i in range(len(data['product_names'][:10]))])
@@ -65,15 +75,23 @@ class TelegramBot:
                 keyboardButtonsRow2.append(InlineKeyboardButton(f"{i+1}", callback_data=f"{data['product_asin'][i]}"))
             keyBoardMarkup = InlineKeyboardMarkup(inline_keyboard=[keyboardButtonsRow1, keyboardButtonsRow2])
             
-            await update.message.reply_text('Scraped products:\n' + scraped_data_message, reply_markup=keyBoardMarkup)
-            await update.message.reply_text('These are the products related to your query, click on the buttons to know more about product.')
+            await update.message.reply_text('These are the products related to your query, click on the buttons to know more about product.\n' + \
+                                                scraped_data_message, reply_markup=keyBoardMarkup)
+
+
+
+    async def get_product_details(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
+        product_asin = update.callback_query.data
+        # product_details = self.driver.get_product_details(product_asin)
+        await update.callback_query.message.reply_text(self.get_product_details_message.format(product_asin=product_asin), 
+                                                       reply_markup=self.reply_markup_remove)
 
 
 
     async def reset_search_query(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         self.queries[update.effective_user.id] = ""
-        await update.message.reply_text(f'Hello {update.effective_user.first_name}\n'
-                                        'Your query has been reset, please tell me what you want to search on Amazon')
+        await update.message.reply_text(self.reset_search_query_message.format(first_name=update.effective_user.first_name),
+                                        reply_markup=self.reply_markup_remove)
 
 
 
@@ -84,14 +102,25 @@ class TelegramBot:
         new_text = update.message.text
         old_text = self.queries[update.effective_user.id]
 
-        print("Text from:", update.effective_user.id, "\nText:", new_text)
+        if new_text == "Yes, Get Products":
+            await self.get_products(update, context)
+            return
+        elif new_text == "No, Update Query":
+            await update.message.reply_text("Please send the follow up message to update your query",
+                                            reply_markup=self.reply_markup_remove)
+            return
+        elif new_text == "Reset Search Query":
+            await self.reset_search_query(update, context)
+            return
+
+        print("Text from:", update.effective_user.username, update.effective_user.id, "\nText:", new_text)
         self.queries[update.effective_user.id] = self.llm.get_user_query(old_query=old_text, new_message=new_text)
+
         if self.queries[update.effective_user.id] != "":
-            await update.message.reply_text(f'{self.queries[update.effective_user.id]}' + 
-                                            f'\n\nIs this what you are looking for {update.effective_user.first_name}?\n'
-                                            'If the following query is correct then please click /get_products\n'
-                                            'if you want to update the query then please send the follow up message\n'
-                                            'and if your query is entirely wrong then please click /reset_search_query')
+            reply_markup_keyboard_buttons = [[KeyboardButton(text="Yes, Get Products"), KeyboardButton(text="No, Update Query")], [KeyboardButton(text="Reset Search Query")]]
+            reply_markup = ReplyKeyboardMarkup(reply_markup_keyboard_buttons, one_time_keyboard=True, resize_keyboard=True)
+            await update.message.reply_text(self.message_handler_message.format(query=self.queries[update.effective_user.id], name=update.effective_user.first_name), \
+                                            reply_markup=reply_markup)
         else:
             await update.message.reply_text("Sorry, I am not able to understand what you are looking for. Please try again")
  
